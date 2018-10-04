852b51672a6db6a8d5e6e3c3ba774b12fc7d4be4
Akhtar Zaman <auz.zamy@gmail.com>
2018-10-04 00:16:10 -0700

    learning node

--- app.js
+++ app.js
@@ -1,23 +1,76 @@
-const http = require('http');
-const fs = require('fs');
+var pocPeople = {
 
-const hostname = '127.0.0.1';
-const port = 3000;
+    firstName: "Andrea"
+    lastName: "Pelayo"
+    age: 20
+};
 
-fs.readFile('index.html', (err, html) => {
-    if(err){
-        throw err;
-    }
+console.log(pocPeople);
 
-    const server = http.createServer((req, res) => {
-        res.statusCode = 200;
-        res.setHeader('content-type', 'text/html');
-        res.write(html);
-        res.end();
-    });
+// What this next line will do is it'll add those two numbers together! Also remember how the function takes in parameters that will be added and return the value.
 
-    server.listen(port, hostname, () => {
+function addNumbers(a,b){
 
-        console.log('server started on port '+port);
-    });
-});
+    return a+b;
+}
+
+console.log(addNumbers(7,5));
+
+// Any function without return 
+
+function andreaPelayo(){
+
+}
+console.log(andreaPelayo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const http = require('http');
+// const fs = require('fs');
+// const hostname = '127.0.0.1';
+// const port = 3000;
+//
+// fs.readFile('index.html', (err, html) => {
+//     if(err){
+//         throw err;
+//     }
+//
+//
+//     const server = http.createServer((req, res) => {
+//         res.statusCode = 200;
+//         res.setHeader('content-type', 'text/html');
+//         res.write(html);
+//         res.end();
+//     });
+//
+//     server.listen(port, hostname, () => {
+//
+//         console.log('server started on port '+port);
+//     });
+// });
